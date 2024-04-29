ab13b0b0e9f5ca67fc16c2275a4d8aee20d16a99
aaron-yang799 <aaron.yang799@gmail.com>
2024-04-29 18:44:27 -0500

    changed background color and change some buttons to links

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,6 +4,13 @@ import Validation from './LoginValidation'
 import axios from 'axios'
 
 function Login() {
+    const backgroundStyle = {
+        width: "100%",
+        height: "100vh", // Or '100vh' for full viewport height
+        backgroundColor: "#81C1FF",
+        backgroundSize: 'cover', // Cover the entire space of the element
+    };
+
     const [values, setValues] = useState({
         email: '',
         password: ''
@@ -33,26 +40,28 @@ function Login() {
     }
 
   return (
-    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-            <h2>Log-In</h2>
-            <form action="" onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email'
-                        onChange={handleInput} className='form-control rounded-0'/>
-                    {errors.email && <p className='text-danger mt-1'> {errors.email}</p>}
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' name='password'
-                        onChange={handleInput} className='form-control rounded-0'/>
-                    {errors.password && <span className='text-danger'> {errors.password}</span>}
-                </div>
-                <button type='submit' className='btn btn-success w-100'><strong>Log In</strong></button>
-                <p className='mb-1 mt-1'>You are agreeing to our terms and policies.</p>
-                <Link to='/signup'className='btn btn-danger border w-100'><strong>Create Account</strong></Link>
-            </form>
+    <div style={backgroundStyle}>
+        <div className='d-flex justify-content-center align-items-center vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2>Log-In</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className='mb-3'>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder='Enter Email' name='email'
+                            onChange={handleInput} className='form-control rounded-0'/>
+                        {errors.email && <p className='text-danger mt-1'> {errors.email}</p>}
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder='Enter Password' name='password'
+                            onChange={handleInput} className='form-control rounded-0'/>
+                        {errors.password && <span className='text-danger'> {errors.password}</span>}
+                    </div>
+                    <button type='submit' className='btn btn-success w-100'><strong>Log In</strong></button>
+                    <p className='mb-1 mt-1'>You are agreeing to our terms and policies.</p>
+                    <a href="/signup" className="float-end mt-2">Create Account</a>
+                </form>
+            </div>
         </div>
     </div>
   )
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -7,6 +7,14 @@ import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Signup() {
+
+    const backgroundStyle = {
+        width: "100%",
+        height: "100vh", // Or '100vh' for full viewport height
+        backgroundColor: "#81C1FF",
+        backgroundSize: 'cover', // Cover the entire space of the element
+    };
+
     const [values, setValues] = useState({
         name: '',
         email: '',
@@ -38,44 +46,46 @@ function Signup() {
     }
 
     return (
-    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-            <h2>Sign-Up</h2>
-            <form action="" onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label htmlFor="name"><strong>Name</strong></label>
-                    <input type="text" placeholder='Enter Name' name='name'
-                        onChange={handleInput} className='form-control rounded-0'/>
-                    {errors.name && <p className='text-danger mt-1'> {errors.name}</p>}
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor="email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email'
-                        onChange={handleInput} className='form-control rounded-0'/>
-                    {errors.email && <p className='text-danger mt-1'> {errors.email}</p>}
-                </div>
-                <div className='mb-3'>
-                    <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' name='password'
-                        onChange={handleInput} className='form-control rounded-0'/>
-                    {errors.password && <span className='text-danger mt-1'> {errors.password}</span>}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="birthday"><strong>Birthday</strong></label>
-                    <DatePicker
-                        selected={values.birthday}
-                        onChange={handleDateChange}
-                        dateFormat="yyyy-MM-dd"
-                        className="form-control rounded-0 ms-2"
-                        name="birthday"
-                        placeholderText={'Enter Birthday'}
-                    />
-                    {errors.birthday && <span className='text-danger mt-1 d-block'> {errors.birthday}</span>}
-                </div>
-                <button type='submit' className='btn btn-danger w-100'><strong>Create Account</strong></button>
-                <p className='mb-1 mt-1'>You are agreeing to our terms and policies.</p>
-                <Link to='/'className='btn btn-success border w-100'><strong>Log In</strong></Link>
-            </form>
+    <div style={backgroundStyle}>
+        <div className='d-flex justify-content-center align-items-center vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2>Sign-Up</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className='mb-3'>
+                        <label htmlFor="name"><strong>Name</strong></label>
+                        <input type="text" placeholder='Enter Name' name='name'
+                            onChange={handleInput} className='form-control rounded-0'/>
+                        {errors.name && <p className='text-danger mt-1'> {errors.name}</p>}
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder='Enter Email' name='email'
+                            onChange={handleInput} className='form-control rounded-0'/>
+                        {errors.email && <p className='text-danger mt-1'> {errors.email}</p>}
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder='Enter Password' name='password'
+                            onChange={handleInput} className='form-control rounded-0'/>
+                        {errors.password && <span className='text-danger mt-1'> {errors.password}</span>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="birthday" className='d-block'><strong>Birthday</strong></label>
+                        <DatePicker
+                            selected={values.birthday}
+                            onChange={handleDateChange}
+                            dateFormat="yyyy-MM-dd"
+                            className="form-control rounded-0"
+                            name="birthday"
+                            placeholderText={'Enter Birthday'}
+                        />
+                        {errors.birthday && <span className='text-danger mt-1 d-block'> {errors.birthday}</span>}
+                    </div>
+                    <button type='submit' className='btn btn-danger w-100'><strong>Create Account</strong></button>
+                    <p className='mb-1 mt-1'>You are agreeing to our terms and policies.</p>
+                    <a href="/signup" className="float-end mt-2">Log In</a>
+                </form>
+            </div>
         </div>
     </div>
   )
